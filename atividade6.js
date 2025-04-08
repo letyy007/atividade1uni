3da172f2258d9626e089ff59b62ed45d23b612b1
lelety2007 <leticiagrazielle258@gmail.com>
2025-04-08 18:31:06 +0000

    concluida

---
+++ atividade6.js
@@ -0,0 +1,12 @@
+    function verificarNumero (numero) {
+        if (numero > 0) {
+            console.log("positivo");
+        } else if (numero < 0) {
+            console.log("negativo");
+        } else {
+            console.log("zero");
+        }
+}    
+verificarNumero(20);
+verificarNumero(-15);
+verificarNumero(0);
